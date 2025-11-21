7d084f480c188e0cc543594a2e0f9ed02a261d5a
kswjdavis <56629316+kswjdavis@users.noreply.github.com>
2025-11-20 20:46:13 -0700

    maybe have bookbot working

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from stats import count_letters
 from stats import chars_list
 import sys
 
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
 
 def main():
-    book_text = get_book_text((sys.argv[1]))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
     num_words = get_nums_words(book_text)
     letter_counts = count_letters(book_text)
     chars_sorted_list = chars_list(letter_counts)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {sys.argv[1]}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
